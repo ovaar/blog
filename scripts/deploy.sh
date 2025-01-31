a0bbcb3bf29f9dd4116dbf02878d6617684cfbaa
ovaar <1405257+ovaar@users.noreply.github.com>
2025-01-31 16:28:43 +0100

    script: Add more git push logic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 docker compose up blog  # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+pushd public
 
 # Add changes to git.
 git add .
@@ -20,4 +20,8 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
+git remote -v
+git push origin master
+
+popd
 git submodule foreach git push origin master
